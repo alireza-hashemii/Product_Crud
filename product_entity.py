833aa2989bd1c1e7943ef76eb66603d15139a3e9
1alirezahashemi1 <pyalireza7@gmail.com>
2022-12-19 00:34:20 +0330

    last changes in files

--- product_entity.py
+++ product_entity.py
@@ -1,21 +1,15 @@
-from abstract_structure import IProduct
 from datetime import datetime
 import json
 
 products = []
 
-
 class DTEncoder(json.JSONEncoder):
     def default(self, obj):
-        # if passed in object is datetime object
-        # convert it to a string
         if isinstance(obj, datetime):
             return str(obj)
-        # otherwise use the default behavior
         return json.JSONEncoder.default(self, obj)
 
-
-class Product(IProduct):
+class Product():
 
     def create(self,Product_id:int,category_id:int,title:str,
     short_description:str,description:str,
@@ -55,6 +49,7 @@ class Product(IProduct):
         else:
             return("No products matched with given id.")
 
+
     def update(self,title:str,short_description:str,description:str,
     slug:str,permalink:str,IsAvailable:bool,sku:str,price:float,
     regular_price:float,sale_price:float,
@@ -93,14 +88,19 @@ class Product(IProduct):
             return("No product were matched with the given id")
                 
 
-
     def delete(self,product_id:int):
-        raise(NotImplementedError)
+        for product in products:
+            if product["product_id"] == product_id:
+                products.remove(product)
+                print("it has succesfully deleted.")
+                return(products)
+        else:
+            return("No product found with the given id")
 
-obj1 = Product()
-dat = datetime(2014,3,4,4,3,2)
-print(obj1.create(3,5,"g","h","h","j","j",True,"d",3.0,3.0,3.0,3,3,True,dat,dat))
-print(obj1.update("gggg","hhhh","hhhh","j","j",True,"d",3.0,3.0,3.0,3,3,True,dat,dat))
-
+# obj1 = Product()
+# dat = datetime(2014,3,4,4,3,2)
+# print(obj1.create(3,5,"g","h","h","j","j",True,"d",3.0,3.0,3.0,3,3,True,dat,dat))
+# print(obj1.update("gggg","hhhh","hhhh","j","j",True,"d",3.0,3.0,3.0,3,3,True,dat,dat))
+# print(obj1.delete(3))
 
 # print(obj1.read(4))
